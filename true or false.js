1d08e230e0ba0234838d3df21c0d0163c0e3befa
Denielss <daniel.martin@escola.pr.gov.br>
2024-05-17 09:58:15 -0300

    novos aprendizados

---
+++ true or false.js
@@ -0,0 +1,22 @@
+//truthy e falsy são tipos primitivos de comparação tambem
+//0 e 1
+//0 eh um valor q o java script considera como falso
+
+console.log(0 == false);//true
+consolo.log(1 == true);//true
+console.log(1 == false);//false
+console.log(0 == true);//false
+
+console.log(""==false);//true
+
+let minhaVar;
+let testnull =null;
+
+console.log(minhaVar);//undefined
+console.log(testnull);//null
+
+let numer = 3;
+let text = "dick";
+
+console.log(typeof(minhaVar));
+console.log(typeof(testnull));
---
+++ var, let e const.js
@@ -0,0 +1,10 @@
+//var
+//pode ser usada antes de ser declarada
+//porem pode aver bugs
+//{tudo q esta dentro da chave , eh um bloco de codigo }
+
+//const 
+//uma ve declarada n pode ser mudada
+
+//let 
+//n pode ser usada sem ates ser declarada
